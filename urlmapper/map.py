d55cbe30b123f8da7d4df0be0e8763cf96070494
pgranger <pgranger@pgranger>
2011-11-05 10:53:30 -0400

    - add gitignore

--- urlmapper/map.py
+++ urlmapper/map.py
@@ -5,44 +5,49 @@ Created on 2011-11-05
 '''
 import re
 
+class UrlNotFound(Exception):
+    pass
+
 class UrlMap(object):
-    
+
     def __init__(self):
-        self.urlmap=[]
-    
-    def add(self,regexp,funct):
-        self.urlmap.append((re.compile(regexp),funct))
-            
-    def map_path(self,path):
-        for regexp,func in self.urlmap:
-            match=regexp.search(path)
+        self.urlmap = []
+
+    def add(self, regexp, func, default=None, name=None):
+        if default is None:
+            default = {}
+        self.urlmap.append((re.compile(regexp), func, default, name))
+
+    def map_path(self, path):
+        for regexp, func, default, name in self.urlmap:
+            match = regexp.search(path)
             if match:
                 kwargs = match.groupdict()
                 if kwargs:
                     args = ()
                 else:
                     args = match.groups()
-                return func(*args,**kwargs)
-        return None    
+                kwargs.update(default)
+                return func, args, kwargs
+        raise UrlNotFound()
 
-def a():
-    return 'a'
-def b():
-    return 'b'
-def c(year,month,day):
-    return year,month,day
-def d(year=None,month=None,day=None):
-    
-    return year,month,day
 
 if __name__ == '__main__':
-    urlmap=UrlMap()
+    def a():
+        return 'a'
+    def b():
+        return 'b'
+    def c(year, month, day):
+        return year, month, day
+    def d(year=None, month=None, day=None):
+        return year, month, day
+
+    urlmap = UrlMap()
     urlmap.add(r'^articles/2003/$', a)
     urlmap.add(r'^articles/(\d{4})/$', b)
     urlmap.add(r'^articles/(\d{4})/(\d{2})/$', c)
-    urlmap.add(r'^articles/(\d{4})/(\d{2})/(\d+)/$', c)
-    urlmap.add(r'^articles/(?P<year>\d{4})/(?P<month>\d{2})/(?P<day>\d{2})/$', d)
-    
-    path='articles/2004/12/21/'
+    #urlmap.add(r'^articles/(\d{4})/(\d{2})/(\d+)/$', c)
+    urlmap.add(r'^articles/(?P<year>\d{4})/(?P<month>\d{2})/(?P<day>\d{2})/$', d, default=dict(param='p'))
+
+    path = 'articles/2004/12/21/'
     print urlmap.map_path(path)
-    
