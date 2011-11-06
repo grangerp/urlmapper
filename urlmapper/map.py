f784a9845e1c18ec6bb759e4c2c3dbf535b172ed
pgranger <pgranger@pgranger>
2011-11-06 08:29:20 -0500

    - add resolvr class

--- urlmapper/map.py
+++ urlmapper/map.py
@@ -8,27 +8,58 @@ import re
 class UrlNotFound(Exception):
     pass
 
+class UrlNotMatch(Exception):
+    pass
+
+class RegexUrlMatch(object):
+    """ class containing a match and all necessary info to call the function """
+    def __init__(self, func, args, kwargs, name=None):
+        self.func = func
+        self.args = args
+        self.kwargs = kwargs
+        self.name = name
+
+    def call(self):
+        """ call function and return result """
+        return self.func(*self.args, **self.kwargs)
+
+
+class RegexUrlPattern(object):
+
+    def __init__(self, regexp, func, kwargs=None, name=None):
+        self.regexp = regexp
+        self.func = func
+        if kwargs is None:
+            kwargs = {}
+        self.kwargs = kwargs
+        self.name = name
+
+    def resolve(self, path):
+        match = self.regexp.search(path)
+        if match:
+            kw = match.groupdict()
+            if kw:
+                args = ()
+            else:
+                args = match.groups()
+            kw.update(self.kwargs)
+            return RegexUrlMatch(self.func, args, kw, self.name)
+        raise UrlNotMatch(path)
+
 class UrlMap(object):
 
     def __init__(self):
         self.urlmap = []
 
-    def add(self, regexp, func, default=None, name=None):
-        if default is None:
-            default = {}
-        self.urlmap.append((re.compile(regexp), func, default, name))
+    def add(self, regexp, func, kwargs=None, name=None):
+        self.urlmap.append(RegexUrlPattern(re.compile(regexp), func, kwargs, name))
 
     def map_path(self, path):
-        for regexp, func, default, name in self.urlmap:
-            match = regexp.search(path)
-            if match:
-                kwargs = match.groupdict()
-                if kwargs:
-                    args = ()
-                else:
-                    args = match.groups()
-                kwargs.update(default)
-                return func, args, kwargs
+        for rp in self.urlmap:
+            try:
+                return rp.resolve(path)
+            except UrlNotMatch:
+                pass
         raise UrlNotFound()
 
 
@@ -39,15 +70,15 @@ if __name__ == '__main__':
         return 'b'
     def c(year, month, day):
         return year, month, day
-    def d(year=None, month=None, day=None):
-        return year, month, day
+    def d(year=None, month=None, day=None, param=None):
+        return year, month, day, param
 
     urlmap = UrlMap()
     urlmap.add(r'^articles/2003/$', a)
     urlmap.add(r'^articles/(\d{4})/$', b)
     urlmap.add(r'^articles/(\d{4})/(\d{2})/$', c)
     #urlmap.add(r'^articles/(\d{4})/(\d{2})/(\d+)/$', c)
-    urlmap.add(r'^articles/(?P<year>\d{4})/(?P<month>\d{2})/(?P<day>\d{2})/$', d, default=dict(param='p'))
+    urlmap.add(r'^articles/(?P<year>\d{4})/(?P<month>\d{2})/(?P<day>\d{2})/$', d)
 
     path = 'articles/2004/12/21/'
-    print urlmap.map_path(path)
+    print urlmap.map_path(path).call()
